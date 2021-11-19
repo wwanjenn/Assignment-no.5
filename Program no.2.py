f4f40d5fb99e84fe127442fce4811b351e7be726
Lemonade-up <wmaglangit1118@gmail.com>
2021-11-19 19:58:11 +0800

    Pseudo Code added

---
+++ Program no.2.py
@@ -0,0 +1,10 @@
+# Program 2: Find the lowest number
+# Create a program that ask 3 numbers. 
+# Find the lowest number using only if-else statement.
+# Display the lowest number
+
+# Steps 
+# 1 Ask for 3 numbers
+
+# 2 If else statments
+# 3 Print
