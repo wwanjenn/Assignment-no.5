769aa145b04b0ad6e4b3d93f254952891a3d7f4c
Lemonade-up <wmaglangit1118@gmail.com>
2021-11-19 18:40:34 +0800

    Expanded the Comments for further Reference of If-Else statements

--- Program no.1.py
+++ Program no.1.py
@@ -4,11 +4,36 @@
 # Grade/Mark: 1.75
 # Description: Very Good
 
+# grade/mark    percentage     description
+# 1.0           97-100          Excellent
+# 1.25          94-96           Excellent
+# 1.5           91-93           Very Good
+# 1.75          88-90           Very Good
+# 2.0           85-87           Good
+# 2.25          82-84           Good
+# 2.50          79-81           Satisfactory
+# 2.75          76-78           Satisfactory
+# 3.0           75              Passing
+# 5.0           65-74           Failure
+# Inc.                          Incomplete
+# W                             Withdrawn
+# D                             Dropeed
+
 #Steps
 #1 Ask for grade percentage
 def inputGrade():
     percentGrade = float(input('Grade Percentage: '))
     return percentGrade
 #2 If else statements
+#2.1
 
+#2.2
+#2.3
+#2.4
+# 2.5
+# 2.6
+# 2.7
+# 2.8
+# 2.9
+# 2.10
 #3 Print
