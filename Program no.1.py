fcb46cbd3012dbf31e75462a311c47ecc2b24a3e
Lemonade-up <wmaglangit1118@gmail.com>
2021-11-19 19:48:51 +0800

    Changed 'or' into 'and' in some If-else statements

--- Program no.1.py
+++ Program no.1.py
@@ -22,7 +22,7 @@
 #Steps
 #1 Ask for grade percentage
 def inputGrade():
-    percentGradeI = input('Grade Percentage: ')
+    percentGradeI = input('Grade Percentage(65-100): ')
     return percentGradeI
 #2 If else statements
 def ifElse(percentGradeC):
@@ -41,43 +41,43 @@ def ifElse(percentGradeC):
     else:
         percentGradeC = float(percentGradeC)
     #2.1
-        if percentGradeC >= 97 or percentGradeC <= 100:
+        if percentGradeC >= 97 and percentGradeC <= 100:
             gradeMarkC = 1.0
             descriptionC = 'Excellent'
     #2.2
-        elif percentGradeC >= 94 or percentGradeC <= 96:
+        elif percentGradeC >= 94 and percentGradeC <= 96:
             gradeMarkC = 1.25
             descriptionC = 'Excellent'
     #2.3
-        elif percentGradeC >= 91 or percentGradeC <= 93:
+        elif percentGradeC >= 91 and percentGradeC <= 93:
             gradeMarkC = 1.5
             descriptionC = 'Very Good'
     #2.4
-        elif percentGradeC >= 88 or percentGradeC <= 90:
+        elif percentGradeC >= 88 and percentGradeC <= 90:
             gradeMarkC = 1.75
             descriptionC = 'Very Good'
     # 2.5
-        elif percentGradeC >= 85 or percentGradeC <= 87:
+        elif percentGradeC >= 85 and percentGradeC <= 87:
             gradeMarkC = 2.0
             descriptionC = 'Good'
     # 2.6
-        elif percentGradeC >= 82 or percentGradeC <= 84:
+        elif percentGradeC >= 82 and percentGradeC <= 84:
             gradeMarkC = 2.25
             descriptionC = 'Good'
     # 2.7
-        elif percentGradeC >= 79 or percentGradeC <= 81:
+        elif percentGradeC >= 79 and percentGradeC <= 81:
             gradeMarkC = 2.5
             descriptionC = 'Satisfactory'
     # 2.8
-        elif percentGradeC >= 76 or percentGradeC <= 78:
+        elif percentGradeC >= 76 and percentGradeC <= 78:
             gradeMarkC = 2.75
             descriptionC = 'Satisfactory'
     # 2.9
-        elif percentGradeC > 74 or percentGradeC <= 76:
+        elif percentGradeC > 74 and percentGradeC <= 76:
             gradeMarkC = 3.0
             descriptionC = 'Passing'
     # 2.10
-        elif percentGradeC >= 65 or percentGradeC <= 74:
+        elif percentGradeC >= 65 and percentGradeC <= 74:
             gradeMarkC = 5.0
             descriptionC = 'Failure'
     return gradeMarkC, descriptionC
