cabca1b185695e2e37957c9c63f0fabf311fb058
Lemonade-up <wmaglangit1118@gmail.com>
2021-11-19 20:20:14 +0800

    Pseudo Code added

---
+++ Program no.3.py
@@ -0,0 +1,13 @@
+# # Program 3: Life stages
+# # Create a program that ask for an age of a person
+# # Display the life stage of the person.
+# # Rules:
+# # 0 - 12 : Kid
+# # 13 - 17 : Teen
+# # 18 : Debut
+# # 19 above : Adult
+
+# Steps
+# 1 Ask for age
+# 2 If else statement with print
+# 3 Call for function
