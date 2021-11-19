1f5327c70e2e1b54dc3937aa211bff3c7696e0f0
Lemonade-up <wmaglangit1118@gmail.com>
2021-11-19 18:34:23 +0800

    Pseudo Code Created

---
+++ Program no.1.py
@@ -0,0 +1,12 @@
+# Create a program that will ask for grade percentage. Display the equivalent Grade/Mark and Description
+# Example:
+# Input grade: 87.6
+# Grade/Mark: 1.75
+# Description: Very Good
+
+#Steps
+#1 Ask for grade percentage
+
+#2 If else statements
+
+#3 Print
